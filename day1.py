7521bb86516e2fbbb43f70484f10697caf4493b6
rohit <rohitcal47@gmail.com>
2024-11-06 02:22:27 +0530

    day 1
    Signed-off-by: rohit <rohitcal47@gmail.com>

--- day1.py
+++ day1.py
@@ -5,3 +5,9 @@ print("Hello World!\nHello World!\nHello World!")
 print("Hello"+" "+"Angela")
 
 input("What is your name?")
+
+print("Hello"+" "+input("What is your name?"))
+
+print("Hello"+" "+input("What is your name?")+"!")
+
+# this is a comment shortcut: ctrl + /
