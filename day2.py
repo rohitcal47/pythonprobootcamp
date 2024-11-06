db7566e589906904c62b9daf65d6e7fd461a6967
rohitcal47 <rohitcal47@gmail.com>
2024-11-07 00:16:43 +0530

    day 1
    Signed-off-by: rohitcal47 <rohitcal47@gmail.com>

---
+++ day2.py
@@ -0,0 +1,20 @@
+# subscripting
+print("Hello"[0])
+print("Hello"[4])
+print("Hello"[-1])
+
+# string
+print("123"+"345")
+
+# integer
+print(123+456)
+
+# large numbers
+print(123_456_789)
+
+# float
+print(3.14159)
+
+# boolean
+print(True)
+print(False)
