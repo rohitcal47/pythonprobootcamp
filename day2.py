d2d239cc0328b18c408348a4e5e73246ae57a859
rohitcal47 <rohitcal47@gmail.com>
2024-11-07 00:30:02 +0530

    day 2
    Signed-off-by: rohitcal47 <rohitcal47@gmail.com>

--- day2.py
+++ day2.py
@@ -4,10 +4,10 @@ print("Hello"[4])
 print("Hello"[-1])
 
 # string
-print("123"+"345")
+print("123" + "345")
 
 # integer
-print(123+456)
+print(123 + 456)
 
 # large numbers
 print(123_456_789)
@@ -18,3 +18,15 @@ print(3.14159)
 # boolean
 print(True)
 print(False)
+
+print(len("Hello Angela"))
+
+# check data type
+print(type("Hello"))
+print(type(12345))
+print(type(True))
+print(type(3.14159))
+
+# type casting / type conversion
+int("12345")
+print(int("123")+int("345"))
