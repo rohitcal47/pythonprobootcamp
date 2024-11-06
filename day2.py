cd572b2cb75f42effaf9b9ae78c8cd455e4cb82c
rohitcal47 <rohitcal47@gmail.com>
2024-11-07 00:46:26 +0530

    day 2
    Signed-off-by: rohitcal47 <rohitcal47@gmail.com>

--- day2.py
+++ day2.py
@@ -30,3 +30,20 @@ print(type(3.14159))
 # type casting / type conversion
 int("12345")
 print(int("123")+int("345"))
+
+int()
+float()
+str()
+bool()
+
+print("Number of letters in your name: "+ str(len(input("Enter your name: "))))
+
+print("My Age is " + str(123))
+print(123 + 456)
+print(123-120)
+print(123*123)
+print(123/17)
+print(123//17) #removes decimal places
+print(123**3) #exponential
+
+# PEMDAS rule is applied, () , **, * , /, +, -
