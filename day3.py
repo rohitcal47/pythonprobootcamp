1e5cdb03d7dde334d580bd0147e9712d2b6ad2b4
rohitcal47 <rohitcal47@gmail.com>
2024-11-08 00:26:20 +0530

    day 2
    Signed-off-by: rohitcal47 <rohitcal47@gmail.com>

--- day3.py
+++ day3.py
@@ -1,13 +1,22 @@
 print("Welcome to the rollercoaster ride!")
+bill=0
 height=float(input("What is your height in centimeters? "))
 if height>=120:
     age=int(input("What is your age? "))
     if age<=12:
-        print("You can take a ride, your ticket price is $5")
+        bill=5
+        print(f"You can take a ride, your ticket price is ${bill}")
     elif age<=18:
-        print("You can take a ride, your ticket price is $7")
+        bill=7
+        print(f"You can take a ride, your ticket price is ${bill}")
     else:
-        print("You can take a ride, your ticket price is $12")
+        bill=12
+        print(f"You can take a ride, your ticket price is ${bill}")
+    want_photo=input("Do you want a Photo? Type Yes or No: ")
+    if want_photo=="Yes":
+        print(f"Your ticket price is ${bill+3}")
+    else:
+        print(f"Your ticket price is ${bill}")
 else:
     print("Sorry, you cannot take a ride")
 
